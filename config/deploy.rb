c38e1b3db78bf2086666cf7ca70dd7e2b0fcdbe5
Alexander Couzens <lynxis@c-base.org>
2011-07-28 23:24:00 +0200

    add capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "dmxacl"
+role :repository, "git://dev.c-base.org/libart-dmxcontrol/django-backend.git"                          # Your HTTP server, Apache/etc
 
-set :scm, :subversion
+set :scm, 'git'
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "baseos.cbrp3.c-base.org"                          # Your HTTP server, Apache/etc
+
+set :use_sudo, false
+set :home_path, "/srv/capistranos/dmxacl"
+set :deploy_to, "/srv/capistranos/dmxacl"
+set :user, "deployer"
+
+namespace :deploy do
+    task :restart do
+    end
+    task :finalize_update do
+    end
+end
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
