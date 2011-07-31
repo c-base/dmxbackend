ef4926689ff50b72c27e6e6230ed5a5e1109d503
Alexander Couzens <lynxis@c-base.org>
2011-07-31 20:03:57 +0200

    tests: add testsideas

--- dmxacl/lightSync/tests.py
+++ dmxacl/lightSync/tests.py
@@ -14,3 +14,11 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+# lightSync.push()
+# - return error when arg is not a list
+# - does not change a light if an empty list posts
+# - change the light [in jsonfile]
+# lightSync.pull()
+# - receive light
+
