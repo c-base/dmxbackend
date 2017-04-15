76596f050e897c72d04dc47eacd4478cf7bfac42
Uwe Kamper <me@uwekamper.de>
2017-04-15 20:04:26 +0200

    run_main with new usb-option

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ~/.virtualenvs/dmxbackend/bin/activate
-python ./main.py /dev/ttyUSB0 tests/mainhall_2017_010.qxw
+python ./main.py tests/mainhall_2017_010.qxw --usb /dev/ttyUSB0
