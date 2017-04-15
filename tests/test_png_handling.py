605efb9c6f4248775af89f431a95f8e4d1372a35
Uwe Kamper <me@uwekamper.de>
2017-04-15 20:00:42 +0200

    reorganize tests

--- tests/test_png_handling.py
+++ tests/test_png_handling.py
@@ -22,14 +22,18 @@ def test_dmx_mapping():
 
 
 class TestRGBMapping:
+    TEST_MODEL = 'Varytec Floodpanel 150'
+    TEST_NAME = 'RGB light'
 
     @pytest.fixture
     def mapping(self):
-        return RGBMapping('RGB light', 0, 0)
+        return RGBMapping(self.TEST_MODEL, self.TEST_NAME, 0, 0)
 
     def test_rgb_mapping(self, mapping):
         assert mapping.pixel == 0
         assert mapping.address == 0
+        assert mapping.model == self.TEST_MODEL
+        assert mapping.name == self.TEST_NAME
 
     def test_map_pixel_to_channels(self, mapping):
         result = mapping.map_pixel_to_channels([(31, 32, 33, 255)])
