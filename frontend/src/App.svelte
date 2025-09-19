04479940ed55f26693f6f1c588b44015f9fac1fc
Uwe Kamper <me@uwekamper.de>
2025-09-19 23:50:59 +0200

    setup svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div class="mainhall-view">
+    
+  </div>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+  .mainhall-view {
+    position: relative;
+    width: 800px;
+    height: 569px;
+    background-color: transparent;
+    background-image: url('/mainhall.png');
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+</style>
