bfc91dba197637ca93b7c2319b4d004bbee94a17
Uwe Kamper <me@uwekamper.de>
2025-09-28 18:30:00 +0200

    gnar

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python ./main.py tests/mainhall_2025_ALL_006.qxw tests/mainhall_2025_ALL_006.ini --usb /dev/ttyUSB0 --mqtt "10.0.1.17" --port 8000
+python ./main.py tests/mainhall_2025_ALL_006.qxw tests/mainhall_2025_ALL_006.ini --usb /dev/ttyUSB0 --port 8000
