d6727b50d4206ee6da5e898bc6210439dbb97734
Uwe Kamper <me@uwekamper.de>
2025-10-05 22:40:01 +0200

    get current run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python ./main.py tests/mainhall_2025_ALL_006.qxw tests/mainhall_2025_ALL_006.ini --usb /dev/ttyUSB0 --port 8000
+python ./main.py tests/mainhall_2025_ALL_006.qxw tests/mainhall_2025_ALL_006.ini \
+        --usb /dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN080082-if00-port0 \
+        --second-usb /dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN080067-if00-port0 \
+        --port 8000
