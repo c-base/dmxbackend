898cde6f88123721bec2246474d1c95cd3a30fb8
Uwe Kamper <me@uwekamper.de>
2025-10-05 23:06:23 +0200

    new fixtures and showfile from cven, pt2

--- run_main.sh
+++ run_main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python ./main.py tests/tests/mainhall_2025_ALL_008_FediDayrcven.qxw tests/tests/mainhall_2025_ALL_008_FediDayrcven.ini \
+python ./main.py tests/mainhall_2025_ALL_008_FediDayrcven.qxw tests/mainhall_2025_ALL_008_FediDayrcven.ini \
         --usb /dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN080082-if00-port0 \
         --second-usb /dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN080067-if00-port0 \
         --port 8000
